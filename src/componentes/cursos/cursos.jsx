fee12028968c84f88b1621a7a686463b3ca3b219
giancarlo <giancarlosossa@gmail.com>
2020-12-02 11:42:33 -0500

    intento fallido

---
+++ src/componentes/cursos/cursos.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+      <div className="cursos">
+        <h2>De Pana Que Si</h2>
+      </div>
+    );
+  }
+  
+  export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Principal from "./Principal"
+import App from "./App"
+import cursos from "./componentes/cursos/cursos"
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <principal />
+    <App />
+    <cursos />
   </React.StrictMode>,
   document.getElementById('root')
 );
